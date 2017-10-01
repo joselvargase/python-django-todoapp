134aecc4dce5c70722ad538b9ae8e361a0da6e6b
Jose Vargas <joselvargas.e@gmail.com>
2017-10-01 10:32:03 -0500

    prient holamundo

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os
 import sys
-
+print("holaMundopythonDjango");
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "todosapp.settings")
     try:
