f336e5f99727d789e80d368c83fb106b04221329
Jose Vargas <joselvargas.e@gmail.com>
2017-10-04 21:52:51 -0500

    ", en setings.installed_apps"

--- CCV/settings.py
+++ CCV/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #'Experimento_1.apps.Experimento_1Config',
-    'Experimento_1.apps.Experimento_1Config'
+    'Experimento_1',
 ]
 
 MIDDLEWARE = [
